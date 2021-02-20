1263c0567c9ed510e1e8f4e42fb95406ccb2229c
candorbloom <candorbloom@naver.com>
2021-02-21 00:54:27 +0900

    FEAT:setting jwt

---
+++ app.js
@@ -0,0 +1,37 @@
+// 의존성 로드
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+// CONFIG 로드
+const config = require('./config')
+const port = process.env.PORT || 3000
+// express 환경설정
+const app = express()
+
+// Json 과 url-encoded query를 분석
+app.use(bodyParser.urlencoded({extended: false}))
+
+// console창에 request log를 print
+app.use(morgan('dev'))
+
+// jwt로 시크릿키 변수를 세팅
+app.set('jwt-secret', config.secret)
+
+// 테스트를 위한 index page
+app.get('/', (req, res) => {
+    res.send('Hello JWT')
+})
+
+// 서버 open
+app.listen(port, ()=> {
+    console.log('Express is running on port ${port}')
+})
+
+// mongoDB 서버 연결
+mongoose.connect(config.mongodbUri)
+const db = mongoose.connection
+db.on('error', console.error)
+db.once('open', ()=>{
+    console.log('conneted to mongodb server')
+})
---
+++ models/user.js
@@ -0,0 +1,38 @@
+const { promises } = require('fs')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    username : String,
+    password: String,
+    admin : {type : Boolean, default:false}
+})
+
+// 새로운 유저레코드(행) 즉 새유저 생성하기
+User.statics.create = function(username,password){
+    const user = new this({
+        username,
+        password
+    })
+    // promises를 반환
+    return user.save()
+}
+
+// username으로 유저 찾기
+User.statics.findOneByUsername = function(username){
+    return this.fidOne({
+        username
+    }).exec()
+}
+
+// 유저레코드의 password 확인하기
+User.methods.verify = function(password){
+    return this.password === password
+}
+
+User.method.assignAdmin = function(){
+    this.admin = true
+    return this.save()
+}
+
+module.exports = mongoose.model('User', User)
